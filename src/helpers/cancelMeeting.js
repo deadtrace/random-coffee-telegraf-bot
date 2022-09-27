10888abf8f1ecbc369834b0aa0d439cb9353ec17
Stanislav Katyushin <justtrace17@gmail.com>
2022-09-27 22:34:56 +0300

    Правки корректной обработки удаления несуществующих встреч

--- src/helpers/cancelMeeting.js
+++ src/helpers/cancelMeeting.js
@@ -4,21 +4,25 @@ import Meeting from "../models/Meeting.js";
 const cancelMeeting = async (ctx, meeetingId) => {
   try {
     const meeting = await Meeting.findById(meeetingId);
-    if (meeting.status === MEETING_STATUSES.CANCELED) {
-      return await ctx.reply("Встреча уже была ранее отменена.");
-    }
-    meeting.status = MEETING_STATUSES.CANCELED;
-    await meeting.save();
 
-    const partnerId =
-      meeting.tid1 === ctx.chat.id ? meeting.tid2 : meeting.tid1;
-    await ctx.reply("Встреча успешно отменена!");
-    await ctx.telegram.sendMessage(
-      partnerId,
-      "К сожалению, коллега отменил встречу на этой неделе."
-    );
+    if (meeting) {
+      if (meeting.status === MEETING_STATUSES.CANCELED) {
+        return await ctx.reply("Встреча уже была ранее отменена.");
+      }
+      meeting.status = MEETING_STATUSES.CANCELED;
+      await meeting.save();
+
+      const partnerId =
+        meeting.tid1 === ctx.chat.id ? meeting.tid2 : meeting.tid1;
+      await ctx.reply("Встреча успешно отменена!");
+      await ctx.telegram.sendMessage(
+        partnerId,
+        "К сожалению, коллега отменил встречу на этой неделе."
+      );
+    }
   } catch (error) {
-    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
   }
 };
 
