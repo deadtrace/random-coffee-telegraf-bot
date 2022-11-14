026034a22e64bbd4731dd8a6860516453fa9f91e
Stanislav Katyushin <justtrace17@gmail.com>
2022-11-14 18:59:20 +0300

    Ссылка с советами при стартовом сообщении

--- src/actions/createProfileAction.js
+++ src/actions/createProfileAction.js
@@ -1,8 +1,19 @@
 import SCENES from "../scenes/scenesList.js";
+import { Markup } from "telegraf";
+import { TEXTS } from "../texts.js";
 
 const createProfileAction = async (ctx) => {
   await ctx.answerCbQuery();
-  await ctx.editMessageReplyMarkup();
+  await ctx.editMessageReplyMarkup({
+    inline_keyboard: [
+      [
+        Markup.button.url(
+          TEXTS.LIFEHACKS_TO_FILL_PROFILE,
+          process.env.LINK_TO_ADVICES
+        ),
+      ],
+    ],
+  });
   ctx.scene.enter(SCENES.CREATE_PROFILE);
 };
 
--- src/commands/start.js
+++ src/commands/start.js
@@ -7,6 +7,12 @@ const startCommand = async (ctx) =>
     reply_markup: {
       inline_keyboard: [
         [Markup.button.callback(TEXTS.FILL_PROFILE, ACTIONS.CREATE_PROFILE)],
+        [
+          Markup.button.url(
+            TEXTS.LIFEHACKS_TO_FILL_PROFILE,
+            process.env.LINK_TO_ADVICES
+          ),
+        ],
       ],
     },
   });
