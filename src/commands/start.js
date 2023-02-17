0a2eda393857e9c098fbc771e12b8f15cfae7e68
Stanislav Katyushin <justtrace17@gmail.com>
2023-02-17 13:13:21 +0300

    Обернул стартовую команд в error catching, чтобы не ломался бот

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,20 +1,26 @@
 import ACTIONS from "../actions/actionsList.js";
 import { TEXTS } from "../texts.js";
 import { Markup } from "telegraf";
+import logError from "../helpers/logError.js";
 
-const startCommand = async (ctx) =>
-  ctx.reply(TEXTS.START, {
-    reply_markup: {
-      inline_keyboard: [
-        [Markup.button.callback(TEXTS.FILL_PROFILE, ACTIONS.CREATE_PROFILE)],
-        [
-          Markup.button.url(
-            TEXTS.LIFEHACKS_TO_FILL_PROFILE,
-            process.env.LINK_TO_ADVICES
-          ),
+const startCommand = async (ctx) => {
+  try {
+    await ctx.reply(TEXTS.START, {
+      reply_markup: {
+        inline_keyboard: [
+          [Markup.button.callback(TEXTS.FILL_PROFILE, ACTIONS.CREATE_PROFILE)],
+          [
+            Markup.button.url(
+              TEXTS.LIFEHACKS_TO_FILL_PROFILE,
+              process.env.LINK_TO_ADVICES
+            ),
+          ],
         ],
-      ],
-    },
-  });
+      },
+    });
+  } catch (error) {
+    logError(error, ctx);
+  }
+};
 
 export default startCommand;
